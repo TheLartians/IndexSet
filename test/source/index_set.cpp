4e76467d164246d9b61c79cbd8e56e0d0c7b79cc
Lars Melchior <TheLartians@users.noreply.github.com>
2020-04-13 21:00:34 +0200

    fix tests

--- test/source/index_set.cpp
+++ test/source/index_set.cpp
@@ -6,31 +6,31 @@
 TEST_CASE("Basic operations") {
   using namespace index_set;
 
-  IndexSet index;
-  index.addIndex(45);
-  CHECK(== index.bits().size());
-  CHECK(index.hasIndex(45));
-  CHECK(!index.hasIndex(46));
-  CHECK(index.hasIndex(index.upperBound()) == false);
-  CHECK(!index.isEmpty());
-  CHECK(index == createIndexSetFromIndex(45));
-  CHECK(index != createIndexSetFromIndex(44));
-  index.add(createIndexSetFromIndices({3, 99}));
-  CHECK(index == createIndexSetFromIndices({3, 45, 99}));
-  index.remove(createIndexSetFromIndices({1, 2, 45, 99}));
-  CHECK(index == createIndexSetFromIndices({3}));
-  index.add(createIndexSetFromIndices({2, 3, 4, 5}));
-  CHECK(index == createIndexSetFromIndices({2, 3, 4, 5}));
-  CHECK(index.count() == 4);
-  index.intersect(createIndexSetFromIndices({0, 4, 5, 6, 7}));
-  CHECK(index == createIndexSetFromIndices({4, 5}));
-  index.clear();
-  CHECK(index == createIndexSetFromIndices({}));
-  CHECK(index.isEmpty());
+  IndexSet indices;
+  indices.addIndex(45);
+  CHECK(indices.upperBound() == indices.bits().size());
+  CHECK(indices.hasIndex(45));
+  CHECK(!indices.hasIndex(46));
+  CHECK(indices.hasIndex(indices.upperBound()) == false);
+  CHECK(!indices.isEmpty());
+  CHECK(indices == createIndexSetFromIndex(45));
+  CHECK(indices != createIndexSetFromIndex(44));
+  indices.add(createIndexSetFromIndices({3, 99}));
+  CHECK(indices == createIndexSetFromIndices({3, 45, 99}));
+  indices.remove(createIndexSetFromIndices({1, 2, 45, 99}));
+  CHECK(indices == createIndexSetFromIndices({3}));
+  indices.add(createIndexSetFromIndices({2, 3, 4, 5}));
+  CHECK(indices == createIndexSetFromIndices({2, 3, 4, 5}));
+  CHECK(indices.count() == 4);
+  indices.intersect(createIndexSetFromIndices({0, 4, 5, 6, 7}));
+  CHECK(indices == createIndexSetFromIndices({4, 5}));
+  indices.clear();
+  CHECK(indices == createIndexSetFromIndices({}));
+  CHECK(indices.isEmpty());
 }
 
 TEST_CASE("Printing") {
   std::stringstream stream;
-  stream << createIndexSetFromIndices({1, 3, 4});
+  stream << index_set::createIndexSetFromIndices({1, 3, 4});
   CHECK(stream.str() == "[1,3,4]");
 }
